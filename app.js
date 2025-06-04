360ed3cca73545e9815ec26992e7e70376eaa01a
Samruddhikhot <samruddhiShrirangkhot@gmail.com>
2025-06-04 17:46:28 +0530

    Add: addTodo function

---
+++ app.js
@@ -0,0 +1,14 @@
+ const todoForm = document.querySelector('form');
+ const todoInput = document.querySelector('#todo-input');
+ const todoListUL = document.querySelector('#todo-list');
+
+ let allTodos =[];
+
+ todoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTodo();
+ })
+
+    function addTodo() {
+        const todoText = todoInput.value.trim();
+        if
